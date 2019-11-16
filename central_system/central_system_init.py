9cb5c56841ba37ceba93b305a9bc424ddfce55aa
diegogonzalezmaneyro <dn.gm88@gmail.com>
2019-11-16 16:19:59 -0300

    connection to openEVSE and OCPP central_system

--- central_system/central_system_init.py
+++ central_system/central_system_init.py
@@ -3,15 +3,30 @@ import websockets
 from datetime import datetime
 
 from ocpp.routing import on, after
-from ocpp.v16 import ChargePoint as cp
-from ocpp.v16.enums import Action, RegistrationStatus, AuthorizationStatus
-from ocpp.v16 import call_result
+from ocpp.v16 import call_result, ChargePoint as cp
+# from ocpp.v16.enums import Action, RegistrationStatus, AuthorizationStatus
+from ocpp.v16.enums import *
 
 #as do not use database, implement an array were you can find
 #all the valid tokens
 valid_tokens = [1234,12345,1111,2222]
 
 class ChargePoint(cp):
+    # ### START TEMPLATE ##
+    # @on(Action.ACTION_NAME)
+    # def on_ACTION_NAME(self, VARS):
+    #     return call_result.ACTION_NAME(
+    #         current_time=datetime.utcnow().isoformat(),
+    #         interval=10,
+    #         status=RegistrationStatus.accepted
+    #     )
+
+    # @after(Action.ACTION_NAME)
+    # def after_ACTION_NAME(self, VARS):
+    #     print("SOMETHING TO PRINT")
+    # ### END TEMPLATE ##
+
+    ## START ACTIONS ##
     @on(Action.BootNotification)
     def on_boot_notification(self, charge_point_vendor, charge_point_model, **kwargs):
         return call_result.BootNotificationPayload(
@@ -44,6 +59,29 @@ class ChargePoint(cp):
     def after_authorize(self, id_tag):
         print("authorization of ", id_tag)
 
+    @on(Action.ChangeAvailability)
+    def on_change_avilability(self, connector_id, av_type):
+        return call_result.ChangeAvailabilityPayload(
+            status=AvailabilityStatus.accepted
+        )
+
+    @after(Action.ChangeAvailability)
+    def after_change_avilability(self, connector_id, type):
+        print("Change avilability ready")
+
+    @on(Action.StartTransaction)
+    def on_start_transaction(self, connector_id, id_tag, meter_start, timestamp):
+        trans_id = 666
+        return call_result.StartTransactionPayload(
+            transaction_id = trans_id,
+            id_tag_info={
+                "status" : AuthorizationStatus.accepted
+            }
+        )
+
+    @after(Action.StartTransaction)
+    def after_start_transaction(self, connector_id, id_tag, meter_start, timestamp):
+        print("STARTED transaction in connector {}, from {}, starting meter: {}, timestamp {}".format(connector_id, id_tag, meter_start, timestamp))
 
 async def on_connect(websocket, path):
     """ For every new charge point that connects, create a ChargePoint instance
