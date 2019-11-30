c3843222657f30c13348a15b77e772967b0e2596
diegogonzalezmaneyro <dn.gm88@gmail.com>
2019-11-30 15:50:48 -0300

    added limit by current and functions to do it

--- charge_point/evse_functions.py
+++ charge_point/evse_functions.py
@@ -24,6 +24,10 @@ TIMEOUT = 1 # secs
 # STOPBITS = serial.STOPBITS_ONE,
 # BYTESIZE = serial.EIGHTBITS
 
+
+#############################################
+################ CONNECTION #################
+#############################################
 def start_connection(serial_name, baud_r, t_out):
     try:
         ## open serial connection
@@ -36,6 +40,10 @@ def start_connection(serial_name, baud_r, t_out):
 
 def end_connetcion(ser):
     ser.close()
+
+#############################################
+################ EVSE STATUS ################
+#############################################
 
 def set_enable(ser, encode=False):
     ## command string to enable EVSE
@@ -106,9 +114,14 @@ def set_disable(ser, encode=False):
     print("DISABLE: ", status)
     return status
 
-def get_energy_usage(ser, encode=False):
-    ## command string to get energy usage
-    COMMAND = "$GU\r"
+#############################################
+############## SET/GET VALUES ###############
+#############################################
+
+def set_current(ser, amps,  encode=False):
+    print("entro")
+    ## command string to set current amps
+    COMMAND = "$SC {}\r".format(amps)
     ## write command on serial
     ## if mac --> encode=True
     if encode:
@@ -124,6 +137,29 @@ def get_energy_usage(ser, encode=False):
         print(line)
         aux = line.split(" ")
     
+    print(aux)
+    status = aux[0]
+    print("SET CURRENT: ", status)
+    return status
+
+def get_energy_usage(ser, encode=False):
+    ## command string to get energy usage
+    COMMAND = "$GU\r"
+    ## write command on serial
+    ## if mac --> encode=True
+    if encode:
+        ser.write(COMMAND.encode())
+        ## read info
+        line = ser.readline()
+        # print(line)
+        aux = line.decode().split(" ")
+    else:
+        ser.write(COMMAND)
+        ## read info
+        line = ser.readline()
+        # print(line)
+        aux = line.split(" ")
+    
     # print(aux)
     status = aux[0]
     # print("status: ", status)
@@ -135,6 +171,70 @@ def get_energy_usage(ser, encode=False):
     print("ENERGY STATUS: {} ; SESSION ENERGY: {} ; GLOBAL ENERGY: {}".format(status, session_energy, global_energy))
 
     return status, session_energy, global_energy
+
+def get_charging_data(ser, encode=False):
+    ## command string to get charging current and voltage
+    COMMAND = "$GG\r"
+    ## write command on serial
+    ## if mac --> encode=True
+    if encode:
+        ser.write(COMMAND.encode())
+        ## read info
+        line = ser.readline()
+        print(line)
+        aux = line.decode().split(" ")
+    else:
+        ser.write(COMMAND)
+        ## read info
+        line = ser.readline()
+        print(line)
+        aux = line.split(" ")
+    
+    print(aux)
+    status = aux[0]
+    print("status: ", status)
+    # session_energy = int(aux[1])/3600000
+    current_AMMETER = int(aux[1])
+    # print("session energy: ", session_energy)
+    # global_energy = int(aux[2].split("^")[0])/1000
+    current_VOLTMETER = int(aux[2].split("^")[0])
+    print("CHARGING STATUS: {} ; CURRENT AMP: {} ; GLOBAL ENERGY: {}".format(status, current_AMMETER, current_VOLTMETER))
+
+    return status, current_AMMETER, current_VOLTMETER
+
+def get_current_settings(ser, encode=False):
+    ## command string to get current settings
+    COMMAND = "$GE\r"
+    ## write command on serial
+    ## if mac --> encode=True
+    if encode:
+        ser.write(COMMAND.encode())
+        ## read info
+        line = ser.readline()
+        # print(line)
+        aux = line.decode().split(" ")
+    else:
+        ser.write(COMMAND)
+        ## read info
+        line = ser.readline()
+        # print(line)
+        aux = line.split(" ")
+    
+    # print(aux)
+    status = aux[0]
+    # print("status: ", status)
+    # session_energy = int(aux[1])/3600000
+    amps = int(aux[1])
+    # print("session energy: ", session_energy)
+    # global_energy = int(aux[2].split("^")[0])/1000
+    flag = int(aux[2].split("^")[0])
+    print("CURRENT STATUS: {} ; AMPS: {} ; FLAG: {}".format(status, amps, flag))
+
+    return status, amps, flag
+
+#############################################
+################# DISPLAY ###################
+#############################################
 
 def set_display_color(ser, color_int=1, encode=False):
     ## command string to get energy usage
@@ -168,16 +268,27 @@ def set_display_text(ser, row, text, encode=False):
         ser.write(COMMAND.encode())
         ## read info
         line = ser.readline()
-        print(line)
+        # print(line)
         aux = line.decode().split("^")
     else:
         ser.write(COMMAND)
         ## read info
         line = ser.readline()
-        print(line)
+        # print(line)
         aux = line.split("^")
 
-    print(aux)
+    # print(aux)
     status = aux[0]
-    print("status: ", status)
+    print("DISPLAY COLOR: ", status)
     return status
+
+#############################################
+################# FUNCTIONS #################
+#############################################
+
+def disable_by_current(current_seted, current_measure):
+    mA_current = current_seted*1000
+    if (current_measure-mA_current)>0:
+        return True
+    else:
+        return False
--- charge_point/serial_connection.py
+++ charge_point/serial_connection.py
@@ -175,6 +175,98 @@ def set_reset(ser, encode=False):
     print("RESET: ", status)
     return status
 
+def get_charging_data(ser, encode=False):
+    ## command string to get charging current and voltage
+    COMMAND = "$GG\r"
+    ## write command on serial
+    ## if mac --> encode=True
+    if encode:
+        ser.write(COMMAND.encode())
+        ## read info
+        line = ser.readline()
+        print(line)
+        aux = line.decode().split(" ")
+    else:
+        ser.write(COMMAND)
+        ## read info
+        line = ser.readline()
+        print(line)
+        aux = line.split(" ")
+    
+    print(aux)
+    status = aux[0]
+    print("status: ", status)
+    # session_energy = int(aux[1])/3600000
+    current_AMMETER = int(aux[1])
+    # print("session energy: ", session_energy)
+    # global_energy = int(aux[2].split("^")[0])/1000
+    current_VOLTMETER = int(aux[2].split("^")[0])
+    print("CHARGING STATUS: {} ; CURRENT AMP: {} ; GLOBAL ENERGY: {}".format(status, current_AMMETER, current_VOLTMETER))
+
+    return status, current_AMMETER, current_VOLTMETER
+
+def get_current_settings(ser, encode=False):
+    ## command string to get current settings
+    COMMAND = "$GE\r"
+    ## write command on serial
+    ## if mac --> encode=True
+    if encode:
+        ser.write(COMMAND.encode())
+        ## read info
+        line = ser.readline()
+        # print(line)
+        aux = line.decode().split(" ")
+    else:
+        ser.write(COMMAND)
+        ## read info
+        line = ser.readline()
+        # print(line)
+        aux = line.split(" ")
+    
+    # print(aux)
+    status = aux[0]
+    # print("status: ", status)
+    # session_energy = int(aux[1])/3600000
+    amps = int(aux[1])
+    # print("session energy: ", session_energy)
+    # global_energy = int(aux[2].split("^")[0])/1000
+    flag = int(aux[2].split("^")[0])
+    print("CURRENT STATUS: {} ; AMPS: {} ; FLAG: {}".format(status, amps, flag))
+
+    return status, amps, flag
+
+def set_current(ser, amps,  encode=False):
+    print("entro")
+    ## command string to set current amps
+    COMMAND = "$SC {}\r".format(amps)
+    ## write command on serial
+    ## if mac --> encode=True
+    if encode:
+        ser.write(COMMAND.encode())
+        ## read info
+        line = ser.readline()
+        print(line)
+        aux = line.decode().split(" ")
+    else:
+        ser.write(COMMAND)
+        ## read info
+        line = ser.readline()
+        print(line)
+        aux = line.split(" ")
+    
+    print(aux)
+    status = aux[0]
+    print("SET CURRENT: ", status)
+    return status
+
+def disable_by_current(current_seted, current_measure):
+    mA_current = current_seted*1000
+    if (current_measure-mA_current)>0:
+        return True
+    else:
+        return False
+
+
 try:
     ## open serial connection
     ser = serial.Serial(SERIAL_NAME,
@@ -183,21 +275,29 @@ try:
     
     print(ser)
     ## get values from openEVSE
-    enable_open_EVSE = set_enable(ser,encode=True)
-    status, session_energy, global_energy = get_energy_usage(ser, encode=True)
-    status = set_display_color(ser, color_int=2,encode=True)
-    disable_open_EVSE = set_disable(ser, encode=True)
-    reset_status = set_reset(ser, encode=True)
+    # enable_open_EVSE = set_enable(ser,encode=True)
+    # status, session_energy, global_energy = get_energy_usage(ser, encode=True)
+    # status = set_display_color(ser, color_int=2,encode=True)
+    # disable_open_EVSE = set_disable(ser, encode=True)
+    # reset_status = set_reset(ser, encode=True)
+    # status = set_display_color(ser ,color_int=7,encode=True)
     # status = set_display_color(ser, color_int=4,encode=True)
     # status = set_display_color(ser ,color_int=5,encode=True)
     # status = set_display_color(ser ,color_int=6,encode=True)
-    # status = set_display_color(ser ,color_int=7,encode=True)
-    text = "APROXIME TARJETA"
-    text_rfid = "RFID: 12345_____"
-    status = set_display_text(ser, 0 ,text ,encode=True)
-    status = set_display_text(ser, 1 ,text_rfid ,encode=True)
+    # text = "APROXIME TARJETA"
+    # text_rfid = "RFID: 12345_____"
+    # status = set_display_text(ser, 0 ,text ,encode=True)
+    # status = set_display_text(ser, 1 ,text_rfid ,encode=True)
     # status = set_display_rfid(ser ,text_rfid ,encode=True)
-    
+    status, seted_amps, flag = get_current_settings(ser, encode=True)
+    status, current_amps, current_volts = get_charging_data(ser, encode=True)
+    current_disable = disable_by_current(seted_amps,current_amps)
+    if current_disable:
+        disable_open_EVSE = set_disable(ser, encode=True)
+
+    # amps_value = 12
+    # status = set_current(ser, amps_value,  encode=True)
+    # status, min_AMPS, max_AMPS = get_current_capacity(ser, encode=True)
 except:
     print("Connection error, check what is going on")
 
